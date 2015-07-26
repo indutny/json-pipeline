75532df128150c14e04b7670dc9abf439c02cffa
Fedor Indutny <fedor@indutny.com>
2015-07-25 20:38:08 -0700

    lib: removing, prepending nodes

--- test/pipeline-test.js
+++ test/pipeline-test.js
@@ -26,4 +26,22 @@ describe('JSON Pipeline', function() {
     p.parse(fixtures.json.p0, 'json');
     assert.deepEqual(p.render('json'), fixtures.json.p0);
   });
+
+  it('should remove nodes and clean up uses', function() {
+    var start = p.add('start');
+    var one = p.add('literal').addLiteral(1);
+    var two = p.add('literal').addLiteral(2);
+    var add = p.add('add', [ one, two ]);
+    var extra1 = p.add('add', [ one, two ]);
+    var ret = p.add('return', [ add ]).setControl(start);
+    var extra2 = p.add('add', [ one, two ]);
+
+    p.remove(extra1);
+    p.remove(extra2);
+
+    assert.equal(one.uses.length, 2);
+    assert.equal(two.uses.length, 2);
+
+    assert.deepEqual(p.render('json'), fixtures.json.p0);
+  });
 });
