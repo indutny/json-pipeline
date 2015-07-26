5521a769c58863865656ec30c54f536adf32290c
Fedor Indutny <fedor@indutny.com>
2015-07-25 20:46:51 -0700

    block/node: `.replace()`, `.insert()`

--- test/pipeline-test.js
+++ test/pipeline-test.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var assertText = require('assert-text');
+
+assertText.options.trim = true;
 
 var pipeline = require('../');
 
@@ -43,5 +46,23 @@ describe('JSON Pipeline', function() {
     assert.equal(two.uses.length, 2);
 
     assert.deepEqual(p.render('json'), fixtures.json.p0);
+  });
+
+  it('should replace node uses with other node', function() {
+    var one = p.add('literal').addLiteral(1);
+    var two = p.add('literal').addLiteral(2);
+    var add = p.add('add', [ one, two ]);
+
+    var three = p.add('literal').addLiteral(3);
+    one.replace(three);
+
+    assertText.equal(p.render('printable'), fixtures.fn2str(function() {/*
+      pipeline {
+        i0 = literal 1
+        i1 = literal 2
+        i2 = add i3, i1
+        i3 = literal 3
+      }
+    */}));
   });
 });
