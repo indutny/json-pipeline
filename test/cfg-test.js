6153d5f790744e179248951c917f084acc4f2fbe
Fedor Indutny <fedor@indutny.com>
2015-07-29 12:25:31 -0700

    cfg: `.reindex()` for testing

--- test/cfg-test.js
+++ test/cfg-test.js
@@ -1,4 +1,7 @@
 var assert = require('assert');
+var assertText = require('assert-text');
+
+assertText.options.trim = true;
 
 var pipeline = require('../');
 
@@ -56,5 +59,99 @@ describe('JSON CFG Builder', function() {
     assert.equal(p.blocks[2].successors.length, 1);
     assert.equal(p.blocks[3].nodes.length, 2);
     assert.equal(p.blocks[3].successors.length, 0);
+  });
+
+  it('should reindex branch', function() {
+    var start = p.block('start');
+
+    var one = p.add('literal').addLiteral(1);
+    var two = p.add('literal').addLiteral(2);
+    var add = p.add('add', [ one, two ]);
+    var branch = p.addControl('if', [ add ]);
+
+    var left = p.jumpFrom(start);
+
+    var x0 = p.add('literal').addLiteral('ok');
+    var leftEnd = p.addControl('jump');
+
+    var right = p.jumpFrom(start);
+
+    var x1 = p.add('literal').addLiteral('not-ok');
+    var rightEnd = p.addControl('jump');
+
+    var merge = p.merge(left, right);
+    var phi = p.addControl('phi', [ x0, x1 ]);
+    p.addControl('return', [ phi ]);
+
+    p.reindex();
+
+    // CFG export
+    var text = p.render({ cfg: true }, 'printable');
+    assertText.equal(text, fixtures.fn2str(function() {/*
+      pipeline {
+        b0 {
+          i0 = literal 1
+          i1 = literal 2
+          i2 = add i0, i1
+          i3 = if ^b0, i2
+        }
+        b0 -> b1, b2
+        b1 {
+          i4 = literal "ok"
+          i5 = jump ^b1
+        }
+        b1 -> b3
+        b2 {
+          i6 = literal "not-ok"
+          i7 = jump ^b2
+        }
+        b2 -> b3
+        b3 {
+          i8 = phi ^b3, i4, i6
+          i9 = return ^b3, i8
+        }
+      }
+    */}));
+  });
+
+  it('should reindex loop', function() {
+    var start = p.block('start');
+    p.addControl('jump');
+
+    var head = p.jumpFrom(start);
+    var read = p.add('read');
+    var branch = p.addControl('if', [ read ]);
+
+    var body = p.jumpFrom(head);
+    p.add('print');
+    body.jump(head);
+
+    var end = p.jumpFrom(head);
+    p.addControl('return', read);
+
+    p.reindex();
+
+    // CFG export
+    var text = p.render({ cfg: true }, 'printable');
+    assertText.equal(text, fixtures.fn2str(function() {/*
+      pipeline {
+        b0 {
+          i0 = jump ^b0
+        }
+        b0 -> b1
+        b1 {
+          i1 = read
+          i2 = if ^b1, i1
+        }
+        b1 -> b2, b3
+        b2 {
+          i3 = print
+        }
+        b2 -> b1
+        b3 {
+          i4 = return ^b3, i1
+        }
+      }
+    */}));
   });
 });
